5f982c58bea709b59a8dd711f3fbdec60903dd2e
Barros313 <gbarros1337@gmail.com>
2024-05-03 21:52:18 -0300

    Added getInteger function with InputMismatchException handle

--- src/Contador.java
+++ src/Contador.java
@@ -1,31 +1,49 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
-import exception.InvalidCountParameterException;
+import exception.ParametrosInvalidosException;
 
 public class Contador {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-
-        System.out.print("Insira o primeiro parâmetro: ");
-        int firstParameter = scan.nextInt();
-
-        System.out.print("Insira o segundo parâmetro: ");
-        int secondParameter = scan.nextInt();
+        int firstParameter = getInteger("Insira o primeiro parâmetro: ", scan);
+        int secondParameter = getInteger("Insira o segundo parâmetro: ", scan);
+        scan.close();
 
         try {
             countParameters(firstParameter, secondParameter);
-        } catch (InvalidCountParameterException e) {
+        } catch (ParametrosInvalidosException e) {
             System.err.println("O segundo parâmetro deve ser maior que o primeiro");
         }
     }
 
-    static void countParameters(int firstParameter, int secondParameter) throws InvalidCountParameterException {
-        if (firstParameter > secondParameter) throw new InvalidCountParameterException();
+    static void countParameters(int firstParameter, int secondParameter) throws ParametrosInvalidosException {
+        if (firstParameter > secondParameter) throw new ParametrosInvalidosException();
 
         int count = secondParameter - firstParameter;
 
-        for (int i = 0; i <= count; i++) {
+        for (int i = 1; i <= count; i++) {
             System.out.println("Imprimindo o número " + i);
         }
+    }
+
+    static int getInteger(String request, Scanner scan) {
+        int output = 0;
+        boolean stillException = false;
+
+        System.out.print(request);
+
+        do {
+            try {
+                stillException = false;
+                output = scan.nextInt();
+            } catch (InputMismatchException e) {
+                stillException = true;
+                System.err.println("Input precisa ser um número inteiro. Tente novamente:");
+                scan.next();
+            }
+        } while (stillException);
+
+        return output;
     }
 }
--- src/exception/InvalidCountParameterException.java
+++
@@ -1,3 +0,0 @@
-package exception;
-
-public class InvalidCountParameterException extends Exception {}
